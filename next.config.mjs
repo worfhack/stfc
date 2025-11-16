c94aa8c45e07d740a45c3faf8394dd17fcaa61b2
tdc <tdecourson@gmail.com>
2025-11-17 00:38:48 +0100

    init3

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const nextConfig = {
     ignoreBuildErrors: true, // coupe le type-check TS en build
   },
   reactStrictMode: true,
-output: "export", // 🔥 clé du build 100% statique
 
   images: {
     domains: ['readdy.ai', 'startrek.local.fr', "demo.startrekfrenchclub.fr"],
